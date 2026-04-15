ffbebfc6d4dc958090b296b5a717f471b60b357c
Omega <emomega@proton.me>
2026-04-15 18:14:07 -0400

    Create mod.rs

---
+++ network/peer/mod.rs
@@ -0,0 +1,10 @@
+pub mod peer;
+pub mod peer_manager;
+pub mod discovery;
+pub mod handshake;
+pub mod reputation;
+pub mod heartbeat;
+pub mod peer_store;
+
+pub use peer::Peer;
+pub use peer_manager::PeerManager;
